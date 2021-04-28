7155b6b215ba26fa2691e257beb7690cc5071e1e
alba054 <prakasamas19h@student.unhas.ac.id>
2021-04-28 11:18:49 +0800

    detail character activity

--- app/src/main/java/com/yoyo/navbottom/main/CharacterDetailActivity.java
+++ app/src/main/java/com/yoyo/navbottom/main/CharacterDetailActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.yoyo.navbottom.R;
@@ -13,25 +15,26 @@ import com.yoyo.navbottom.main.characterRecycler.CharacterModel;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CharacterDetailActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        civCharacterPoster = findViewById(R.id.iv_poster_detail);
-        tvName = findViewById(R.id.tv_char_name);
-        setContentView(R.layout.activity_character_detail);
-    }
-
-    private CircleImageView civCharacterPoster;
+    private ImageView civCharacterPoster;
     private TextView tvName;
 
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        civCharacterPoster = (ImageView)findViewById(R.id.iv_poster_detail);
+        tvName = (TextView)findViewById(R.id.tv_char_name);
+        setContentView(R.layout.activity_character_detail);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
+        civCharacterPoster = (ImageView)findViewById(R.id.iv_poster_detail);
+        tvName = (TextView)findViewById(R.id.tv_char_name);
         CharacterModel characterModel = getIntent().getParcelableExtra("CHARACTER_DETAIL");
+        Log.d("debug :", Integer.toString(characterModel.getPoster()));
 
-        civCharacterPoster.setImageResource(characterModel.getPoster());
         tvName.setText(characterModel.getName());
         getSupportActionBar().setTitle(characterModel.getName());
     }
